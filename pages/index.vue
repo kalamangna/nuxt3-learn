7e180dfb5c70f3db169800747523a582bdb63bac
Abd Dzuljalali Wal Ikram <35287914+kalamangna@users.noreply.github.com>
2023-02-15 22:18:45 +0800

    add products

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis iste incidunt assumenda earum at debitis nihil
+      delectus unde voluptas dicta?
+    </p>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis iste incidunt assumenda earum at debitis nihil
+      delectus unde voluptas dicta?
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <Head>
+      <Title>Nuxt Dojo | {{ product.title }}</Title>
+      <Meta name="description" :content="product.description" />
+    </Head>
+
+    <ProductDetails :product="product" />
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const { id } = route.params
+
+const { data: product } = await useFetch(`https://fakestoreapi.com/products/${id}`)
+
+if (!product.value) {
+  throw createError({ statusCode: 404, statusMessage: 'Product not found', fatal: true })
+}
+
+definePageMeta({
+  layout: 'products'
+})
+
+</script>
+
+<style scoped>
+
+</style>
